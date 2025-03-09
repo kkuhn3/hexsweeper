ea208b6fd0dbe833291025c6c950ee96ee97a314
kkuhn3 <karljkuhn@gmail.com>
2025-03-08 23:21:58 -0500

    triangle support

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ let livesleft = 3;
 let flagsleft = 99;
 let revealsleft = WIDTH * HEIGHT - flagsleft;
 const hexratio = 1.6;
+const triangleratio = 1.155;
 let mode = 4;
 
 function loadDivs() {
     GAMESTARTED = false;
     grid.innerHTML = "";
+    livestotal = lifesSetting.value;
     setLivesLeft(lifesSetting.value);
     setFlagsLeft(bombSetting.value);
     WIDTH = widthSetting.value;
@@ -18,36 +20,47 @@ function loadDivs() {
     revealsleft = WIDTH * HEIGHT - flagsleft;
     results.innerHTML = "";
     settings.style.display = "none";
-    if (hexSetting.checked) {
-        mode = 6;
-    }
-    else {
-        mode = 4;
-    }
 
     grid.style.gridTemplateColumns = "repeat(" + WIDTH + ", 1fr)";
     grid.style.gridTemplateRows = "repeat(" + HEIGHT + ", 1fr)";
-    if (mode === 4) {
+    if (triSetting.checked) {
+        mode = 3;
         grid.style.marginLeft = "0px";
         grid.style.marginRight = "0px";
-        grid.style.paddingRight = "0px";
+        grid.style.paddingRight = "calc((80dvw / (" + WIDTH * triangleratio + ")))";
         grid.style.paddingBottom = "0px";
     }
-    else {
+    else if (hexSetting.checked) {
+        mode = 6;
         grid.style.marginLeft = "-7.5dvw";
         grid.style.marginRight = "-7.5dvw";
         grid.style.paddingRight = "calc((95dvw / (" + WIDTH * hexratio * 5 + ")))";
         grid.style.paddingBottom = "calc((95dvw / (" + WIDTH * hexratio * 2 + ")))";
+    }
+    else {
+        mode = 4;
+        grid.style.marginLeft = "0px";
+        grid.style.marginRight = "0px";
+        grid.style.paddingRight = "0px";
+        grid.style.paddingBottom = "0px";
     }
 
     for (let i = 0; i < HEIGHT; i++) {
         for (let j = 0; j < WIDTH; j++) {
             const div = document.createElement("div");
             div.classList.add("cell", "unrevealed");
-            if (mode === 4) {
-                div.style.fontSize = "calc(80dvw / (2 * " + WIDTH + "))";
+            if (mode === 3) {
+                div.classList.add("triangle");
+                if ((i % 2 === 1 && j % 2 === 0) ||
+                    (i % 2 === 0 && j % 2 === 1)) {
+                     div.classList.add("down");
+                }
+                div.style.marginRight = "-1000px";
+                div.style.marginBottom = "calc((80dvw / (" + WIDTH * triangleratio * 16 + ")))"
+                div.style.width = "calc((80dvw / (" + WIDTH * triangleratio * .5 + ")))";
+                div.style.fontSize = "calc(80dvw / (" + WIDTH * 1.5 + "))";
             }
-            else {
+            else if (mode == 6) {
                 div.classList.add("hex");
                 if (i % 2 === 1) {
                     div.classList.add("odd-row");
@@ -55,6 +68,9 @@ function loadDivs() {
                 div.style.marginBottom = "calc((95dvw / (" + WIDTH * hexratio * -2.1 + ")))";
                 div.style.width = "calc((95dvw / (" + WIDTH * hexratio + ")))";
                 div.style.fontSize = "calc(95dvw / (" + 3 * WIDTH + "))";
+            }
+            else {
+                div.style.fontSize = "calc(80dvw / (2 * " + WIDTH + "))";
             }
             div.id = `cell-${j}-${i}`;
             div.onmousedown = (e) => {mouseDown(div, e);};
@@ -144,7 +160,7 @@ function startGame(div) {
         const randomX = Math.floor(Math.random() * WIDTH);
         const randomY = Math.floor(Math.random() * HEIGHT);
         const randomDiv = document.getElementById(`cell-${randomX}-${randomY}`);
-        if (!neighbors.includes(randomDiv) && !randomDiv.classList.contains("bomb")) {
+        if (div != randomDiv && !neighbors.includes(randomDiv) && !randomDiv.classList.contains("bomb")) {
             randomDiv.classList.add("bomb");
             bombs--;
         }
@@ -161,7 +177,7 @@ function setLivesLeft(num) {
             if (i < livesleft) {
                 lifes.innerHTML += "💖";
             }
-            else {
+            else if (i < livestotal) {
                 lifes.innerHTML += "💔";
             }
         }
@@ -176,18 +192,21 @@ function getNeighbors(div) {
     const x = parseInt(div.id.split("-")[1]);
     const y = parseInt(div.id.split("-")[2]);
     let divs = []
-    if (mode === 4) {
-        for (let i = x-1; i <= x+1; i++) {
-            for (let j = y-1; j <= y+1; j++) {
-                if ((i != x || j != y) && 
-                    i >= 0 && i < WIDTH && 
-                    j >= 0 && j < HEIGHT) {
-                    divs.push(document.getElementById(`cell-${i}-${j}`));
-                }
+    if (mode === 3) {
+        let neighbors = [
+            [-1, 0],
+            [1, 0],
+            [0, div.classList.contains("down") ? -1 : 1]
+        ];
+        for (const [dx, dy] of neighbors) {
+            const newX = x + dx;
+            const newY = y + dy;
+            if (newX >= 0 && newX < WIDTH && newY >= 0 && newY < HEIGHT) {
+                divs.push(document.getElementById(`cell-${newX}-${newY}`));
             }
         }
     }
-    else {
+    else if (mode === 6) {
         const isOddRow = y % 2 === 1;
         const neighbors = [
             [isOddRow ? 0 : -1, -1],  // top left
@@ -203,6 +222,17 @@ function getNeighbors(div) {
             const newY = y + dy;
             if (newX >= 0 && newX < WIDTH && newY >= 0 && newY < HEIGHT) {
                 divs.push(document.getElementById(`cell-${newX}-${newY}`));
+            }
+        }
+    }
+    else {
+        for (let i = x-1; i <= x+1; i++) {
+            for (let j = y-1; j <= y+1; j++) {
+                if ((i != x || j != y) && 
+                    i >= 0 && i < WIDTH && 
+                    j >= 0 && j < HEIGHT) {
+                    divs.push(document.getElementById(`cell-${i}-${j}`));
+                }
             }
         }
     }
@@ -325,11 +355,11 @@ function revealAllCells(div) {
 function flagCell(div) {
     if (div.classList.contains("unrevealed")) {
         if (div.classList.contains("flag")) {
-            div.classList.remove("flag");
             if (!div.classList.contains("boom")) {
                 div.innerHTML = "";
+                div.classList.remove("flag");
+                setFlagsLeft(flagsleft + 1);
             }
-            setFlagsLeft(flagsleft + 1);
         }
         else  {
             div.classList.add("flag");
